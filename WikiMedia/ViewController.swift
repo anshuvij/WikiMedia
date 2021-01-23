22ca3cb1419792daf7e858f05106b31ba2ae76f0
Anshu Vij <anshu.vij@sustlabs.com>
2021-01-23 20:30:28 +0530

    Initial Commit

---
+++ WikiMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WikiMedia
+//
+//  Created by Mobile Apps Team on 1/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
